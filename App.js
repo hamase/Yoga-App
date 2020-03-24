a0bc0732bfa9f124dc83ffb7ed0be677d0c0f146
Hamase Shahabbaspour <hamase86@gmail.com>
2020-03-24 22:21:48 +0430

    An App for coach Yoga in home

---
+++ App.js
@@ -0,0 +1,83 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <ImageBackground
+        source={require('./assets/images/mainBack.jpg')}
+        style={styles.mainBackground}>
+        <View style={styles.flexEnd}>
+          <View style={styles.mainText}>
+            <Text style={styles.mainTextTitle}>YOGA</Text>
+            <Text style={styles.mainTextDesc}>
+              A yoga teacher in your pocket
+            </Text>
+            <TouchableOpacity style={styles.mainBtn}>
+              <Text style={styles.mainBtnText}>Get Started</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainBackground: {
+    flex: 1,
+    resizeMode: 'center',
+  },
+  flexEnd: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  mainText: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    height: '40%',
+    marginBottom: 60,
+  },
+  mainTextTitle: {
+    color: '#fff',
+    fontSize: 65,
+    fontFamily: 'sans-serif-medium',
+  },
+  mainTextDesc: {
+    color: '#fff',
+    fontSize: 20,
+    fontFamily: 'sans-serif-medium',
+    width: '35%',
+    marginBottom: 40,
+  },
+  mainBtn: {
+    backgroundColor: '#8147de',
+    paddingVertical: 10,
+    paddingHorizontal: 60,
+    borderRadius: 15,
+  },
+  mainBtnText: {
+    color: '#fff',
+    fontSize: 18,
+    fontFamily: 'sans-serif-medium',
+  },
+});
+
+export default App;
